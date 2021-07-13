1b6b51ad465e76f0d3d848f601caeb0b4b5bd571
Angus Farquhar <mr.angus.farquhar@gmail.com>
2021-07-13 18:52:50 +1000

    pinger added

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"time"
 )
 
+// Routines
 func f(n int) {
 	for i := 0; i < 10; i++ {
 		fmt.Println(n, ":", i)
@@ -14,10 +15,40 @@ func f(n int) {
 	}
 }
 
-func main() {
-	for i := 0; i < 10; i++ {
-		go f(i)
+// Channels
+func pinger(c chan string) {
+	for i := 0; ; i++ {
+		c <- "ping"
 	}
+}
+
+func ponger(c chan string) {
+	for i := 0; ; i++ {
+		c <- "pong"
+	}
+}
+
+func printer(c chan string) {
+	for {
+		msg := <-c
+		fmt.Println(msg)
+		time.Sleep(time.Second * 1)
+	}
+}
+
+func main() {
+	// for i := 0; i < 10; i++ {
+	// 	go f(i)
+	// }
+	// var input string
+	// fmt.Scanln(&input)
+
+	var c chan string = make(chan string)
+
+	go pinger(c)
+	go printer(c)
+	go ponger(c)
+
 	var input string
 	fmt.Scanln(&input)
 }
